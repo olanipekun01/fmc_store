4049105afeff0045cdeca6009f58e703fc8083e6
olanipekun01 <56523566+olanipekun01@users.noreply.github.com>
2023-09-11 11:36:01 -0700

    Update settings.py

--- fmc_store/settings.py
+++ fmc_store/settings.py
@@ -112,6 +112,8 @@ MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+STATIC_ROOT = '/home/Eyitayo/fmc_store/staticfiles_build/static'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
